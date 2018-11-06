33220b10b354cb62a01db2fdd911bd942d4d95c6
tan90 <tan-90@outlook.com>
2018-11-06 08:07:42 -0200

    Disable send button when sending message.
    The contact form would keep the send button active while sending. Now it's disabled and uses a default cursor while a message is being sent.

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -151,6 +151,7 @@ export default class Contact extends React.Component
                         className={Style.SubmitButton}
                         type='submit'
                         value='SEND MESSAGE'
+                        disabled={this.state.sending}
                     />
                 </form>
 
