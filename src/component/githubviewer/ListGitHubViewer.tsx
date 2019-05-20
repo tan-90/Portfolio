87998b8b7088d59dc21d4ff6a65e94ed81c7ce07
tan90 <tan-90@outlook.com>
2019-05-20 17:41:25 -0300

    Add default project viewer styling.
    The original idea was to make the child component send data to the page header.
    That way the page header could react to the displayed project.
    That will require a different approach to state management and code that I don't have the time to write now.
    The current solution is just a collapsible list of projects.
    Fix a few styling mistakes that don't need a separate commit.

--- src/component/githubviewer/ListGitHubViewer.tsx
+++ src/component/githubviewer/ListGitHubViewer.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React from 'react';
 import ReactMarkdown  from 'react-markdown';
 
@@ -8,21 +9,31 @@ import { GitHubViewer } from '../../element/GitHubViewer';
 import { IPropsGitHubViewerComponent } from '../../element/GitHubViewer';
 import { ILayoutStyle } from '../../layout/LayoutStyle';
 import { LayoutComponent } from '../../layout/LayoutComponent';
+import { IStateLayoutComponent } from '../../layout/LayoutComponent';
 import { LayoutRegistry } from '../../layout/LayoutRegistry';
 import { Styles } from '../../layout/Styles';
 
 import DefaultListGitHubViewer from '../../style/githubviewer/DefaultListGitHubViewer.scss';
 
+interface IListGitHubViewerState extends IStateLayoutComponent
+{
+    visible: boolean[];
+}
+
 interface IListGitHubViewerStyle extends ILayoutStyle
 {
     gitHubViewer: string;
+    repository: string;
+    hidden: string;
+    header: string;
+    content: string;
 }
 
 @Styles<IListGitHubViewerStyle>(
     { name: 'Default', data: DefaultListGitHubViewer }
 )
 @ComponentProvider(GitHubViewer, true)
-export class ListGitHubViewer extends LayoutComponent<IPropsGitHubViewerComponent>
+export class ListGitHubViewer extends LayoutComponent<IPropsGitHubViewerComponent, IListGitHubViewerState>
 {
     public constructor(props: IPropsGitHubViewerComponent)
     {
@@ -30,9 +41,29 @@ export class ListGitHubViewer extends LayoutComponent<IPropsGitHubViewerComponen
 
         const { manager } = this.props;
 
+        let visibleArray: boolean[] = [];
+        if (this.props.repositories)
+        {
+            visibleArray = this.props.repositories.map(() => false);
+        }
+
         this.state = {
-            activeStyle: manager.getActiveStyle(ListGitHubViewer.name)
+            activeStyle: manager.getActiveStyle(ListGitHubViewer.name),
+
+            visible: visibleArray
         };
+
+        this.handleHeaderClick = this.handleHeaderClick.bind(this);
+    }
+
+    private handleHeaderClick(index: number): void
+    {
+        const { visible } = this.state;
+        visible[index] = !visible[index];
+
+        this.setState({
+            visible
+        });
     }
 
     public render(): ReactNode
@@ -43,31 +74,53 @@ export class ListGitHubViewer extends LayoutComponent<IPropsGitHubViewerComponen
             <div className={style.gitHubViewer}>
                 {
                     (!this.props.error && !this.props.repositories) &&
-                    <span>Loading...</span>
+                    <span>Loading ...</span>
                 }
 
                 {
                     (this.props.error) &&
 
-                    <span>Oh noes! We have an error ):</span>
+                    <span>Oh noes! We have an error ):<br/>Maybe try again later?</span>
                 }
 
                 {
                     (this.props.repositories) &&
-                    <ul>
-                        {
-                            this.props.repositories.map(repository => {
-                                return (
-                                    <li key={repository.name}>
-                                        <h1>{repository.name}</h1>
-                                        <h2>{repository.description}</h2>
+                    this.props.repositories.map((repository, index) => {
+                        return (
+                            <div
+                                className={classNames({
+                                    [style.repository]: true,
+                                    [style.hidden]: !this.state.visible[index]
+                                })}
+                                key={repository.name}
+                            >
+                                {/* UNDO UNTIL THIS DISSAPEARS */}
+                                <div className={style.header} onClick={() => this.handleHeaderClick(index)}>
+                                    {
+                                        this.state.visible[index] ?
+                                        (
+                                            <i className={'material-icons'}>keyboard_arrow_down</i>
+                                        ) :
+                                        (
+                                            <i className={'material-icons'}>keyboard_arrow_right</i>
+                                        )
+                                    }
 
-                                        <ReactMarkdown source={repository.readme}/>
-                                    </li>
-                                );
-                            })
-                        }
-                    </ul>
+                                    <h1>{repository.name}</h1>
+                                    <h2>{repository.description}</h2>
+                                </div>
+
+                                <div className={style.content}>
+                                    <ReactMarkdown
+                                        source={repository.readme}
+                                        renderers={{
+                                            link: props => <a {...props} target={'_blanc'}/>
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        );
+                    })
                 }
             </div>
         );
